38c03ec30e9d7ecca4d21b21f1648b6c3246b99f
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-08-16 17:23:38 -0400

    Initial commit, with some unit tests.

---
+++ Harmful.Test/TestFromCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Harmful.Test {
+    [TestClass]
+    public class TestFromCode {
+        [TestMethod]
+        public void TestFromLatinCharacter() {
+            ushort lead, trail;
+            Assert.AreEqual(0, Utf16.FromCode(0x7a, out lead, out trail));
+
+            Assert.AreEqual(0x7a, lead);
+            Assert.AreEqual(0, trail);
+
+            Assert.AreEqual("\u007A", Utf16.FromCode(0x7A));
+        }
+
+        [TestMethod]
+        public void TestHighSingleComponent() {
+            ushort lead, trail;
+            Assert.AreEqual(0, Utf16.FromCode(0x6c34, out lead, out trail));
+
+            Assert.AreEqual(0x6c34, lead);
+            Assert.AreEqual(0, trail);
+
+            Assert.AreEqual("\u6C34", Utf16.FromCode(0x6C34));
+        }
+
+        [TestMethod]
+        public void TestNonBasic() {
+            ushort lead, trail;
+            Assert.AreEqual(2, Utf16.FromCode(0x1D11E, out lead, out trail));
+
+            Assert.AreEqual(0xD834, lead);
+            Assert.AreEqual(0xDD1E, trail);
+
+            Assert.AreEqual("\uD834\uDD1E", Utf16.FromCode(0x1D11E));
+        }
+    }
+}
---
+++ Harmful/Utf16.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Harmful {
+    /// <summary>
+    /// Contains methods for dealing with UTF-16 character data.
+    /// </summary>
+    public static class Utf16 {
+        /// <summary>
+        /// Converts a Unicode code point to a UTF-16 string
+        /// (native C# string).
+        /// </summary>
+        public static string FromCode(int code) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines the lead and trail components of the surrogate pair
+        /// for the Unicode character specified by code point.
+        /// </summary>
+        /// <param name="code">The code point of the character to encode.</param>
+        /// <param name="lead">The lead/first surrogate.</param>
+        /// <param name="trail">
+        /// The trail/last surrogate, or null if the character was encoded in
+        /// a single 16-bit integer.
+        /// </param>
+        /// <returns>
+        /// Either 1 or 2, depending how many 16-bit units were needed to encode the
+        /// character. If 1, the 'trail' component will be 0; only the 'lead' is relevant.
+        /// </returns>
+        public static int FromCode(int code, out ushort lead, out ushort trail) {
+            throw new NotImplementedException();
+        }
+    }
+}
